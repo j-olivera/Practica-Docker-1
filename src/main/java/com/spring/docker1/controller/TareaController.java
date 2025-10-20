eafeb3570191812b84687571b97e47384620571b
ju4n_0 <juanswc87@gmail.com>
2025-10-20 20:25:06 -0300

    Estructura - 3

--- src/main/java/com/spring/docker1/controller/TareaController.java
+++ src/main/java/com/spring/docker1/controller/TareaController.java
@@ -1,4 +1,59 @@
 package com.spring.docker1.controller;
 
+import com.spring.docker1.dto.TareaRequestDTO;
+import com.spring.docker1.dto.TareaResponseDTO;
+import com.spring.docker1.mapper.TareaMapper;
+import com.spring.docker1.model.Tarea;
+import com.spring.docker1.service.TareaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/tarea")
 public class TareaController {
+
+    @Autowired
+    private TareaService tareaService;
+    @Autowired
+    private TareaMapper tareaMapper;
+
+    //PUT
+    @PutMapping("/create")
+    public ResponseEntity<TareaResponseDTO> putTarea(@RequestBody TareaRequestDTO tareaRequestDTO){
+        Tarea tarea = tareaMapper.toEntity(tareaRequestDTO);
+        Tarea nuevaTarea = tareaService.save(tarea);
+        TareaResponseDTO tareaResponseDTO = tareaMapper.toResponse(nuevaTarea);
+        return ResponseEntity.status(HttpStatus.CREATED).body(tareaResponseDTO);
+    }
+    //GET
+    @GetMapping("/id/{id}")
+    public ResponseEntity<TareaResponseDTO> getTareaById(@PathVariable Long id){
+        Tarea tarea = tareaService.findById(id);
+        TareaResponseDTO tareaResponseDTO = tareaMapper.toResponse(tarea);
+        return ResponseEntity.status(HttpStatus.OK).body(tareaResponseDTO);
+    }
+    @GetMapping("/allTares")
+    public ResponseEntity<List<TareaResponseDTO>> getAllTareas(){
+        List<Tarea> tareas = tareaService.findAll();
+        List<TareaResponseDTO> tareaResponseDTO = tareas.stream()
+                .map(tareaMapper::toResponse)
+                .toList();
+        return ResponseEntity.ok(tareaResponseDTO);
+    }
+    //
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<String> deleteTareaById(@PathVariable Long id){
+        Tarea tarea = tareaService.findById(id);
+        if(tarea != null){
+            tareaService.deleteById(id);
+            return ResponseEntity.ok("Tarea has been deleted");
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
